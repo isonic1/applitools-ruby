f3ca388c75a9d9772e5efd5afa39d7964ccc2d80
isonic1 <justin@Justins-MBP.home>
2017-08-02 12:05:51 -0400

    initial commit

---
+++ test.rb
@@ -0,0 +1,32 @@
+require 'eyes_selenium'
+
+describe 'Testing Applitools' do
+
+  before(:each) do |e|
+    browser = Selenium::WebDriver.for :chrome
+    @eyes = Applitools::Selenium::Eyes.new
+    @eyes.api_key = EVN['APPLITOOLS_KEY']
+    @eyes.match_level = Applitools::MATCH_LEVEL[:strict]
+    @eyes.force_full_page_screenshot = true
+    @eyes.stitch_mode = :css
+    @driver = @eyes.open(driver: browser, app_name: "Full Page Screenshot", test_name: e.full_description, viewport_size: { width: 1000, height: 800 })
+  end
+
+  after(:each) do
+    @eyes.abort_if_not_closed
+    @driver.quit
+  end
+
+  it 'Applitools' do
+    @driver.get 'https://applitools.com'
+    begin
+      @eyes.check_window 'CNN'
+    rescue
+      puts "Encontered error. Retrying..."
+      retry
+    end
+    results = @eyes.close(false)
+    expect(results.passed?).to eq true
+  end
+
+end
