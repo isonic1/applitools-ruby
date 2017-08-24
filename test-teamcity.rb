66b5a340c91d1d5e23ded2b5cea1ece17ff7381e
isonic1 <justin@Justins-MBP.home>
2017-08-24 17:00:44 -0400

    new file

---
+++ test-teamcity.rb
@@ -0,0 +1,49 @@
+require 'eyes_selenium'
+
+describe 'Testing Applitools' do
+
+  before(:all) do
+    @eyes = Applitools::Selenium::Eyes.new
+    @eyes.api_key = ENV['APPLITOOLS_KEY']
+    @eyes.match_level = Applitools::MATCH_LEVEL[:strict]
+    @eyes.force_full_page_screenshot = true
+    @eyes.stitch_mode = :css
+    batch_info = Applitools::BatchInfo.new(ENV['JOB_NAME'])
+    batch_info.id = ENV["APPLITOOLS_BATCH_ID"]
+    @eyes.batch = batch_info
+  end
+
+  before(:each) do |e|
+    browser = Selenium::WebDriver.for :chrome
+    @driver = @eyes.open(driver: browser, app_name: "Full Page Screenshot", test_name: e.full_description)
+  end
+
+  after(:each) do
+    @eyes.abort_if_not_closed
+    @driver.quit
+  end
+
+  it 'Applitools Test' do
+    @driver.get 'https://applitools.com'
+    begin
+      @eyes.check_window 'Applitools'
+    rescue
+      puts "Encontered error. Retrying..."
+      retry
+    end
+    results = @eyes.close(false)
+    expect(results.passed?).to eq true
+  end
+
+  it 'CNN' do
+    @driver.get 'https://cnn.com'
+    begin
+      @eyes.check_window 'CNN'
+    rescue
+      puts "Encontered error. Retrying..."
+      retry
+    end
+    results = @eyes.close(false)
+    expect(results.passed?).to eq true
+  end
+end
