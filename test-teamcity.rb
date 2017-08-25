f7d95bfe62fefab2f61e8df8457ad95225c51ddd
isonic1 <justin@Justins-MBP.home>
2017-08-25 11:24:51 -0400

    updated teamcity

--- test-teamcity.rb
+++ test-teamcity.rb
@@ -5,10 +5,11 @@ describe 'Testing Applitools' do
   before(:all) do
     @eyes = Applitools::Selenium::Eyes.new
     @eyes.api_key = ENV['APPLITOOLS_KEY']
+    puts "MY APPLITOOLS KEY: #{ENV['APPLITOOLS_KEY']}"
     @eyes.match_level = Applitools::MATCH_LEVEL[:strict]
     @eyes.force_full_page_screenshot = true
     @eyes.stitch_mode = :css
-    batch_info = Applitools::BatchInfo.new(ENV['JOB_NAME'])
+    batch_info = Applitools::BatchInfo.new(ENV['APPLITOOLS_BATCH_NAME'])
     batch_info.id = ENV["APPLITOOLS_BATCH_ID"]
     @eyes.batch = batch_info
   end
@@ -25,25 +26,20 @@ describe 'Testing Applitools' do
 
   it 'Applitools Test' do
     @driver.get 'https://applitools.com'
-    begin
-      @eyes.check_window 'Applitools'
-    rescue
-      puts "Encontered error. Retrying..."
-      retry
-    end
+    @eyes.check_window 'Applitools'
     results = @eyes.close(false)
     expect(results.passed?).to eq true
   end
 
-  it 'CNN' do
-    @driver.get 'https://cnn.com'
-    begin
-      @eyes.check_window 'CNN'
-    rescue
-      puts "Encontered error. Retrying..."
-      retry
-    end
-    results = @eyes.close(false)
-    expect(results.passed?).to eq true
-  end
+  # it 'CNN' do
+  #   @driver.get 'https://cnn.com'
+  #   begin
+  #     @eyes.check_window 'CNN'
+  #   rescue
+  #     puts "Encontered error. Retrying..."
+  #     retry
+  #   end
+  #   results = @eyes.close(false)
+  #   expect(results.passed?).to eq true
+  # end
 end
