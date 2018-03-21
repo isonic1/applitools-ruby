200543185affd876ac58e23611d1ce55754b2721
isonic1 <justin@Justins-MBP.fios-router.home>
2018-03-21 17:39:28 -0400

    updated for cloud

--- test-jenkins.rb
+++ test-jenkins.rb
@@ -4,45 +4,33 @@ describe 'Testing Applitools' do
   
   before(:all) do
     @eyes = Applitools::Selenium::Eyes.new
-    @eyes.api_key = ENV['APPLITOOLS_KEY']
+    @eyes.api_key = ENV['APPLITOOLS_API_KEY']
     @eyes.match_level = Applitools::MATCH_LEVEL[:strict]
     @eyes.force_full_page_screenshot = true
     @eyes.stitch_mode = :css
-    batch_info = Applitools::BatchInfo.new(ENV['JOB_NAME'])
-    batch_info.id = ENV["APPLITOOLS_BATCH_ID"]
-    @eyes.batch = batch_info
+    if ENV['JENKINS_HOME']
+      batch_info = Applitools::BatchInfo.new(ENV['JOB_NAME'])
+      batch_info.id = ENV["APPLITOOLS_BATCH_ID"]
+      @eyes.batch = batch_info
+    end
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome()
+    caps['platform'] = 'Windows 7'
+    caps['version'] = '63.0'
+    caps['screenResolution'] = '2560x1600'    
+    sauce_user = ENV['SAUCE_USER']
+    sauce_key = ENV['SAUCE_KEY']
+    @driver = Selenium::WebDriver.for(:remote, :url => "https://#{sauce_user}:#{sauce_key}@ondemand.saucelabs.com:443/wd/hub", :desired_capabilities => caps)
   end
   
-  before(:each) do |e|
-    browser = Selenium::WebDriver.for :chrome
-    @driver = @eyes.open(driver: browser, app_name: "Full Page Screenshot", test_name: e.full_description)
-  end
-
-  after(:each) do
+  after(:all) do
     @eyes.abort_if_not_closed
     @driver.quit
   end
 
-  it 'Applitools Test' do
+  it 'Applitools Test' do |e|
+    @eyes.open(driver: @driver, app_name: "Full Page Screenshot", test_name: e.full_description, viewport_size: {width: 1050, height: 700})
     @driver.get 'https://applitools.com'
-    begin
-      @eyes.check_window 'Applitools'
-    rescue
-      puts "Encontered error. Retrying..."
-      retry
-    end
-    results = @eyes.close(false)
-    expect(results.passed?).to eq true
-  end
-
-  it 'CNN' do
-    @driver.get 'https://cnn.com'
-    begin
-      @eyes.check_window 'CNN'
-    rescue
-      puts "Encontered error. Retrying..."
-      retry
-    end
+    @eyes.check_window 'Applitools'
     results = @eyes.close(false)
     expect(results.passed?).to eq true
   end
